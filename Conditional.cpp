085dc824b6d7a2d85e5c66719d9f0ccbad0bb731
pratiknawale93 <pratiknawale93@gmail.com>
2026-08-07 16:21:52 +0530

    Add files via upload

---
+++ Conditional.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+
+//    int  age;
+//    cout<<"Enter your Age :"<<endl;
+//    cin>>age;
+
+//    if (age>=18)
+//    {
+//     cout<<"Your are Eligible to vote !";
+//    }
+//    else{
+//     cout<<"Note eligible to vote"<<endl;
+//    }
+
+
+//  int num1=10, num2=20;
+//  if(num1>num2)
+//  {
+//     cout<<"The num1 is greater";
+//  }
+//  else{
+//     cout<<"The num2 is greater";
+
+//  }
+
+//     int no;
+//     cout<<"Enter your no :";
+//     cin>>no;
+//     if(no%2==0)
+//     {
+//         cout<<"The no is even";
+//     }
+//     else
+//     {
+//         cout<<"The no is odd";
+//     }
+
+
+
+
+
+}
+
+
+
+
